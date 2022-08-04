98e49f62dae4f18c8539ffc77acb25a1afd2daf7
Clarky <clarky117@outlook.com>
2022-08-04 13:20:55 +0800

    departments being added successfully

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,24 @@
+// import dotenv
+try{
+    require('dotenv').config();
+} catch (error){
+    console.log(error);
+}
+
+// import mysql
+const mysql = require('mysql2/promise');
+
 // connect to mysql
+function connect(){
+    return mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: 'employee_db',
+    })
+}
+
+// export connect function
+module.exports = {
+    connect,
+}
--- models/department.js
+++ models/department.js
@@ -1,12 +1,19 @@
 // create functions to export
 
-function addDepartment(name){
+// import connection
+const { connect } = require("../db/connection")
+
+async function addDepartment(name){
     // add a department, 
-    
+    const db = await connect();
+    await db.query('INSERT INTO `employee_db`.`departments` (`name`) VALUES (?)', name);    
 }
 
-function getDepartments(){
+async function getDepartments(){
     // view all departments,
+    const db = await connect();
+    const [departments] = await db.query('SELECT * FROM departments');
+    return departments;
 
 }
 
